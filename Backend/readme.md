56f79581d625ce3f2e0485350d307bd912264218
AnonMuk <zubair.mukhi@gmail.com>
2019-04-30 17:27:17 -0400

    Updates, I think

--- Backend/readme.md
+++ Backend/readme.md
@@ -6,3 +6,10 @@
 * bodyParser
 ## Architecture
 * Built on ExpressJS backend run RESTfully. 
+* app.js ties into routes.js, which pulls config from data/config.js
+* routes.js hands all data in JSON format to RESTful URLs based on SQL queries 
+##Setup
+```
+npm install express mysql body-parser request
+npm init
+```
--- Backend/routes/routes.js
+++ Backend/routes/routes.js
@@ -3,18 +3,19 @@ const pool = require ('../data/config');
 const router = app => {
     app.get('/', (request, response) => {
         response.send({
-            message: 'Node.js and Express REST API'
+            message: 'Noteable: Stationery in Motion'
         });
     });
 
+    // ********USERS********
 	app.get('/users', (request, response) => {
 		pool.query('SELECT * FROM user', (error, result) => {
 			if (error) throw error;
 			response.send(result);
 		});
 	});
-/*	app.post('/users', (request, response) => {
-		pool.query('INSERT INTO users SET ?', request.body,
+	app.post('/users', (request, response) => {
+		pool.query('INSERT INTO user SET ?', request.body,
 			(error, result) => {
 				if (error) throw error;
 				response.status(201).send
@@ -24,7 +25,7 @@ const router = app => {
 
 	app.put('/users/:id', (request, response) => {
 		const id = request.params.id;
-		pool.query('UPDATE users SET ? WHERE id = ?', [request.body, id]
+		pool.query('UPDATE user SET ? WHERE id = ?', [request.body, id]
 		, (error, result) => {
 			if(error) throw error;
 			response.send('User updated successfully.');
@@ -33,7 +34,7 @@ const router = app => {
 
 	app.delete('/users/:id', (request, response) => {
 		 const id = request.params.id;
-		pool.query('DELETE FROM users WHERE id = ?', id,
+		pool.query('DELETE FROM user WHERE id = ?', id,
 		 (error, result) => {
 			if (error) throw error;
 			response.send('User deleted.');
@@ -42,12 +43,58 @@ const router = app => {
 
 	app.get('/users/:id', (request, response) => {
 		const id = request.params.id;
-		pool.query('SELECT * FROM users WHERE id = ?', id, 
+		pool.query('SELECT * FROM user WHERE id = ?', id, 
 			(error, result) => {
 			if (error) throw error;
 			response.send(result);
 		});
-	});*/
+    });
+
+
+    // ************IMPLEMENT COLLECTION ****************
+
+    app.get('/ImpColl', (request, response) => {
+        pool.query('SELECT * FROM Implement_Collection', (error, result) => {
+            if (error) throw error;
+            response.send(result);
+        });
+    });
+    app.post('/ImpColl', (request, response) => {
+        pool.query('INSERT INTO Implement_Collection SET ?', request.body,
+            (error, result) => {
+                if (error) throw error;
+                response.status(201).send
+                    ('Implement Collection added with ID: ${result.insertID}');
+            });
+    });
+
+    app.put('/ImpColl/:id', (request, response) => {
+        const id = request.params.id;
+        pool.query('UPDATE Implement_Collection SET ? WHERE IC_ID = ?', [request.body, id]
+            , (error, result) => {
+                if (error) throw error;
+                response.send('Implement Collection updated successfully.');
+            });
+    });
+
+    app.delete('/ImpColl/:id', (request, response) => {
+        const id = request.params.id;
+        pool.query('DELETE FROM Implement_Collection WHERE IC_ID = ?', id,
+            (error, result) => {
+                if (error) throw error;
+                response.send('Implement Collection deleted.');
+            });
+    });
+
+    app.get('/ImpColl/:id', (request, response) => {
+        const id = request.params.id;
+        pool.query('SELECT * FROM Implement_Collection WHERE IC_ID = ?', id,
+            (error, result) => {
+                if (error) throw error;
+                response.send(result);
+            });
+    });
+
 }
 
 module.exports = router;
