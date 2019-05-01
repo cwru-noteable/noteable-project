6d1b9b036457999e5a51eb94b68a3d480f40bffc
AnonMuk <zubair.mukhi@gmail.com>
2019-04-30 22:40:28 -0400

    Added functionality to OtherCollection and started adding comments

--- Backend/data/config.js
+++ Backend/data/config.js
@@ -1,14 +1,17 @@
 const port = 3306;
 const mysql = require('mysql');
 
+//connection information for server
 const config = {
-	host: 'eecslab-9.case.edu',
-	user: 'team_7',
-	password: 'e6444a4a',
+    host: 'eecslab-9.case.edu',
+    user: 'team_7',
+    password: 'e6444a4a',
     database: 'team_7',
     multipleStatements: true
 };
 
+//connection pool as opposed to single connection (a little bit friendlier for multi-user setups)
 const pool = mysql.createPool(config);
 
+//make this available to routes
 module.exports = pool;
--- Backend/routes/routes.js
+++ Backend/routes/routes.js
@@ -1,5 +1,7 @@
-const pool = require ('../data/config');
+//get the server config from ../data/config.js
+const pool = require('../data/config');
 
+//Homepage JSON message
 const router = app => {
     app.get('/', (request, response) => {
         response.send({
@@ -8,12 +10,16 @@ const router = app => {
     });
 
     // ********USERS********
+
+    // list all users [COMPLETE]
 	app.get('/users', (request, response) => {
 		pool.query('SELECT * FROM user', (error, result) => {
 			if (error) throw error;
 			response.send(result);
 		});
-	});
+    });
+
+    // add a user [INCOMPLETE]
 	app.post('/users', (request, response) => {
 		pool.query('INSERT INTO user SET ?', request.body,
 			(error, result) => {
@@ -23,6 +29,7 @@ const router = app => {
 		});
 	});
 
+    //edit a user [INCOMPLETE]
 	app.put('/users/:id', (request, response) => {
 		const id = request.params.id;
 		pool.query('UPDATE user SET ? WHERE id = ?', [request.body, id]
@@ -32,6 +39,7 @@ const router = app => {
 		});
 	});
 
+    //delete a user [INCOMPLETE]
 	app.delete('/users/:id', (request, response) => {
 		 const id = request.params.id;
 		pool.query('DELETE FROM user WHERE id = ?', id,
@@ -41,6 +49,7 @@ const router = app => {
 		});
 	});
 
+    //display a specific user [COMPLETE]
 	app.get('/users/:id', (request, response) => {
 		const id = request.params.id;
 		pool.query('SELECT * FROM user WHERE id = ?', id, 
@@ -53,12 +62,15 @@ const router = app => {
 
     // ************IMPLEMENT COLLECTION ****************
 
+    //get table of users to implement collections [COMPLETE]
     app.get('/ImpColl', (request, response) => {
         pool.query('SELECT * FROM Implement_Collection', (error, result) => {
             if (error) throw error;
             response.send(result);
         });
     });
+
+    //edit table of implement collections [INCOMPLETE]
     app.post('/ImpColl', (request, response) => {
         pool.query('INSERT INTO Implement_Collection SET ?', request.body,
             (error, result) => {
@@ -68,6 +80,7 @@ const router = app => {
             });
     });
 
+    //update table of implement collections [INCOMPLETE]
     app.put('/ImpColl/:id', (request, response) => {
         const id = request.params.id;
         pool.query('UPDATE Implement_Collection SET ? WHERE IC_ID = ?', [request.body, id]
@@ -77,6 +90,7 @@ const router = app => {
             });
     });
 
+    //delete table of implement collections [INCOMPLETE]
     app.delete('/ImpColl/:id', (request, response) => {
         const id = request.params.id;
         pool.query('DELETE FROM Implement_Collection WHERE IC_ID = ?', id,
@@ -86,40 +100,44 @@ const router = app => {
             });
     });
 
+    //get a list of items (currently selecting names) in an implement collection [COMPLETE] (weird null set handling)
     app.get('/ImpColl/:id', (request, response) => {
         const id = request.params.id;
-        var responses = [];
+        var aggregate = [];
         pool.query('SELECT FountainP.FP_Name FROM (Implement_Collection natural join IC_FP natural join FountainP) WHERE Implement_Collection.IC_ID = ? ', id,
             (error, result) => {
                 if (error) throw error;
-                responses.push(result);
+                aggregate.push(result);
                 pool.query('SELECT CartridgeP.CP_Name FROM (Implement_Collection natural join IC_CP natural join CartridgeP) WHERE Implement_Collection.IC_ID = ? ', id,
                     (error, result) => {
                         if (error) throw error;
-                        responses.push(result);
+                        aggregate.push(result);
                         pool.query('SELECT WoodP.WP_Name FROM (Implement_Collection natural join IC_WP natural join WoodP) WHERE Implement_Collection.IC_ID = ? ', id,
                             (error, result) => {
                                 if (error) throw error;
-                                responses.push(result);
+                                aggregate.push(result);
                                 pool.query('SELECT MechanicalP.MP_Name FROM (Implement_Collection natural join IC_MP natural join MechanicalP ) WHERE Implement_Collection.IC_ID = ? ', id,
                                     (error, result) => {
                                         if (error) throw error;
-                                        responses.push(result);
-                                        response.send(responses);
+                                        aggregate.push(result);
+                                        response.send(aggregate);
                                     });
                             });
                     });
             });
     });
-    //inner join IC_FP on IC_FP.IC_ID = Implement_Collection.IC_ID) inner join FountainP on IC_FP.FP_ID = FountainP.FP_ID)
+ 
     // ************OTHER COLLECTION****************
 
+    //list all Other Collections [COMPLETE]
     app.get('/OthColl', (request, response) => {
         pool.query('SELECT * FROM Other_Collection', (error, result) => {
             if (error) throw error;
             response.send(result);
         });
     });
+
+    //add Other Collection [INCOMPLETE]
     app.post('/OthColl', (request, response) => {
         pool.query('INSERT INTO Other_Collection SET ?', request.body,
             (error, result) => {
@@ -129,6 +147,7 @@ const router = app => {
             });
     });
 
+    //edit Other Collection [INCOMPLETE]
     app.put('/OthColl/:id', (request, response) => {
         const id = request.params.id;
         pool.query('UPDATE Other_Collection SET ? WHERE OC_ID = ?', [request.body, id]
@@ -138,6 +157,7 @@ const router = app => {
             });
     });
 
+    //delete Other Collection [INCOMPLETE]
     app.delete('/OthColl/:id', (request, response) => {
         const id = request.params.id;
         pool.query('DELETE FROM Other_Collection WHERE OC_ID = ?', id,
@@ -147,12 +167,36 @@ const router = app => {
             });
     });
 
+    //get a list of items in an other collection [COMPLETE] (weird null set handling)
     app.get('/OthColl/:id', (request, response) => {
         const id = request.params.id;
-        pool.query('SELECT * FROM ((Other_Collection inner join OC_L on OC_L.OC_ID=Other_Collection.OC_ID) inner join Lead on OC_L.L_ID = Lead.L_ID) WHERE Other_Collection.OC_ID = ?', id, (error, result) => {
-            if (error) throw error;
-            response.send(result);
-        });
+        var aggregate = [];
+        pool.query('SELECT Lead.L_Name FROM (Other_Collection natural join OC_L natural join Lead) WHERE Other_Collection.OC_ID = ? ', id,
+            (error, result) => {
+                if (error) throw error;
+                aggregate.push(result);
+                pool.query('SELECT Replacements.R_Name FROM (Other_Collection natural join OC_R natural join Replacements) WHERE Other_Collection.OC_ID = ? ', id,
+                    (error, result) => {
+                        if (error) throw error;
+                        aggregate.push(result);
+                        pool.query('SELECT Ink.I_Name FROM (Other_Collection natural join OC_I natural join Ink) WHERE Other_Collection.OC_ID = ? ', id,
+                            (error, result) => {
+                                if (error) throw error;
+                                aggregate.push(result);
+                                pool.query('SELECT Pen_Cartridge.PC_Name FROM (Other_Collection natural join OC_PC natural join Pen_Cartridge) WHERE Other_Collection.OC_ID = ? ', id,
+                                    (error, result) => {
+                                        if (error) throw error;
+                                        aggregate.push(result);
+                                        pool.query('SELECT Utility.U_Name FROM (Other_Collection natural join OC_U natural join Utility) WHERE Other_Collection.OC_ID = ? ', id,
+                                            (error, result) => {
+                                                if (error) throw error;
+                                                aggregate.push(result);
+                                                response.send(aggregate);
+                                            });
+                                    });
+                            });
+                    });
+            });
     });
 
 }
