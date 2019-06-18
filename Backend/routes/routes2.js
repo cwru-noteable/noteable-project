b6249a10065d7bebf425f564202f4cb5c2a3a9f6
AnonMuk <zubair.mukhi@gmail.com>
2019-06-17 23:20:52 -0500

    Cutting out the middleman

--- Backend/routes/routes2.js
+++ Backend/routes/routes2.js
@@ -18,8 +18,25 @@ const router = app => {
    ██    ███████ ███████    ██
 */
 app.get('/mechP/:id', (request, response) => {
-  response.send(asker.getMechP(request.params.id));
+  response.send(getMechP(request.params.id));
 });
+
+function getMechP(userID){
+  pool.query('SELECT * FROM (Collections natural join Items natural join MechanicalP) WHERE Collections.UserID = ?'), userID, (error, result) => {
+    var frontend = result.map((item, i) => ({
+      basicAtts:{
+        itemName: item.MP_Name,
+        itemId: item.MP_ID,
+        manufacturer: item.MP_Manufacturer,
+        type: item.type,
+      },
+      stats: {
+        material: item.MP_Material,
+        leadSize: item.MP_Lead_Size,
+      }
+    }));
+  }
+}
 
 
 /*
