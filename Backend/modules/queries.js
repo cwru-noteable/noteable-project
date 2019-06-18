ee50c3e8d01363b8955515f3071522ae41d4fa06
AnonMuk <zubair.mukhi@gmail.com>
2019-06-17 23:00:33 -0500

    NOW I fixed brackets

--- Backend/modules/queries.js
+++ Backend/modules/queries.js
@@ -1,18 +1,20 @@
 const pool = require('../data/config2');
 const mysql = require(mysql);
 
-function getMechP (userID){
-  pool.query('SELECT * FROM (Collections natural join Items natural join MechanicalP) WHERE Collections.UserID = ?', userID, (error, result) => {
-      frontend = result.map((item, i) => ({
-          basicAtts: {
-              itemName: item.MP_Name,
-              itemId: item.MP_ID,
-              manufacturer: item.MP_Manufacturer,
-              type: item.type,
-          },
-          stats: {
-              material: item.MP_Material,
-              leadSize: item.MP_Lead_Size,
-          }
-      }));
+
+function getMechP(userID){
+  pool.query('SELECT * FROM (Collections natural join Items natural join MechanicalP) WHERE Collections.UserID = ?'), userID, (error, result) => {
+    var frontend = result.map((item, i) => ({
+      basicAtts:{
+        itemName: item.MP_Name,
+        itemId: item.MP_ID,
+        manufacturer: item.MP_Manufacturer,
+        type: item.type,
+      },
+      stats: {
+        material: item.MP_Material,
+        leadSize: item.MP_Lead_Size,
+      }
+    }));
+  }
 }
